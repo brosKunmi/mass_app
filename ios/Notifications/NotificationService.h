7879f0f76766970b6f220c75200d0f9cf93272d6
brosKunmi <kakinyele@gmail.com>
2022-06-08 08:53:51 +0100

    first version completed

---
+++ ios/Notifications/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  Notifications
+//
+//  Created by macbook on 09/03/2022.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
